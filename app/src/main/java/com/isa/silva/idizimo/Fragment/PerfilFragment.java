29e37bb8cbe8993a5c829dcbecda3838f37890eb
isabelle silva de oliveira <isabelle.oliveira@redfox.tech>
2019-10-30 11:51:22 -0300

    ajuste perfil

--- app/src/main/java/com/isa/silva/idizimo/Fragment/PerfilFragment.java
+++ app/src/main/java/com/isa/silva/idizimo/Fragment/PerfilFragment.java
@@ -10,6 +10,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.isa.silva.idizimo.R;
 import com.isa.silva.idizimo.Utils.Util;
 
@@ -18,6 +22,8 @@ public class PerfilFragment extends Fragment {
     private EditText edt_mail;
     private EditText edt_tel;
     private EditText edt_nome;
+    private Button btn_alterar;
+    private FirebaseAuth firebase = new FirebaseAuth(FirebaseApp.getInstance());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,10 +33,26 @@ public class PerfilFragment extends Fragment {
         edt_tel = view.findViewById(R.id.edt_tel);
         edt_nome = view.findViewById(R.id.edt_user);
 
+        btn_alterar = view.findViewById(R.id.btn_alterar);
 
-        edt_nome.setText("teste");
+        final FirebaseUser user = firebase.getCurrentUser();
+
+
+        edt_nome.setText(user.getDisplayName());
         edt_nome.setEnabled(false);
         edt_tel.addTextChangedListener(Util.mask(edt_tel, Util.FORMAT_FONE));
+        edt_tel.setText(user.getPhoneNumber());
+        edt_mail.setText(user.getEmail());
+
+        btn_alterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.updateEmail(edt_mail.getText().toString());
+            }
+        });
+
+
+
 
 
         return view;
